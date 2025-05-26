f2b81abde99dd7afaadf643a307da97ed9141af4
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-05-26 13:46:51 +0200

    Fixed lantern rotation issue

--- ShowOff/Assets/Scripts/Player/ObjectPlayerFollow.cs
+++ ShowOff/Assets/Scripts/Player/ObjectPlayerFollow.cs
@@ -7,6 +7,9 @@ public class ObjectPlayerFollow : MonoBehaviour
     [SerializeField]
     Transform player;
 
+    [SerializeField]
+    bool shouldRotate = false;
+
     Vector3 offset;
 
     void Start()
@@ -17,5 +20,10 @@ public class ObjectPlayerFollow : MonoBehaviour
     void LateUpdate()
     {
         transform.position = player.position + player.rotation * offset;
+
+        if (shouldRotate)
+        {
+            transform.rotation = player.rotation;
+        }
     }
 }
